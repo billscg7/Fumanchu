2be5e93911402c8e1f5f05817a8f06f178ced9b6
billscg7 <billscg7@gmail.com>
2023-02-20 23:38:13 -0500

    mi primer controlador

---
+++ app/Http/Controllers/VentasController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class VentasController extends Controller
+{
+    public function listar(){
+        //funcion listar
+        return view('welcome');
+    }
+
+    
+    public function listar2(){
+        //funcion listar
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\VentasController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/prueba', function () {
+Route::get('/prueba2', function () {
     return view('welcome');
 });
+
+Route::get('/prueba3', [VentasController::class, 'listar'] );
